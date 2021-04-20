f5facdd029bdf0c7b129bfbc4a3aa7f451dbfe02
argmnt <yagizgazibaba1996@hotmail.com>
2021-04-20 16:15:55 +0300

    fix

--- src/main/java/com/yagizgazibaba/jwt_boilerplate/auth/AuthController.java
+++ src/main/java/com/yagizgazibaba/jwt_boilerplate/auth/AuthController.java
@@ -10,6 +10,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
@@ -22,7 +23,7 @@ public class AuthController {
     private final UserService userService;
 
     @PostMapping("/login")
-    public ResponseEntity<JWTToken> login(LoginDTO loginDTO) {
+    public ResponseEntity<JWTToken> login(@RequestBody LoginDTO loginDTO) {
         JWTToken jwtToken = userService.createJWTToken(loginDTO);
         return ResponseEntity.ok(jwtToken);
     }
@@ -33,7 +34,7 @@ public class AuthController {
      * @param authentication
      * @return
      */
-    @GetMapping("user")
+    @GetMapping("/user")
     public UserDetails getUser(Authentication authentication) {
         JwtAuthenticationToken token = (JwtAuthenticationToken) authentication;
         Map<String, Object> attributes = token.getTokenAttributes();
